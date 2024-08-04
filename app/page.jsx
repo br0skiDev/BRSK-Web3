d3db75b4b5dfbabe85715fc13a47ce0353ec2aa3
Moreno <moreno-rizvic@outlook.de>
2024-08-04 05:55:09 +0200

    commit

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
         <div className="absolute left-2 bottom-2 flex flex-col text-slate-950">
         <div className="flex gap-2 items-center">
             <Image src={"/assets/sepolia.png"} alt="" width={34} height={34} />
-            <h1 className="font-bold text-md text-emerald-300">Sepolia ETH Netzwerk</h1>
+            <h1 className="font-bold text-md text-emerald-300">SepoliaETH</h1>
         </div>
             <div className="w-fit h-fit select-none p-2 rounded-lg backdrop-blur-lg drop-shadow-xl text-xs font-light text-emerald-100">
                 Token Contract: <span className="select-all font-black text-emerald-250 text-[10.5pt] ml-2">0x529bBdF560b5b3F5467b47F1B86E9805e4bC1e60</span>
