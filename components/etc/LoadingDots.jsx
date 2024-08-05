466a2443de7565e4dcabd4205e7de5061b35ce26
Moreno <moreno-rizvic@outlook.de>
2024-08-05 09:39:46 +0200

    commit

--- components/etc/LoadingDots.jsx
+++ components/etc/LoadingDots.jsx
@@ -2,11 +2,11 @@ import { motion } from "framer-motion";
 
 export const LoadingDots = () => {
   return (
-    <div className="flex bg-slate-500/10 rounded-full mt-2 py-2 px-4">
+    <div className="flex bg-slate-400/10 rounded-full mt-2 py-[11px] px-4 drop-shadow-lg">
       {[0, 1, 2].map((index) => (
         <motion.div
           key={index}
-        className="w-2 h-2 rounded-full bg-red-500 mx-[7.5px]"
+        className="w-2 h-2 rounded-full bg-red-500 mx-[7px]"
           animate={{
             scale: [1, 1.5, 1],
             transition: {
